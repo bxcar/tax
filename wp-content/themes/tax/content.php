66695fa95670c65161a67939cc011f9623cf6701
bxcar <malanchukdima@mail.ru>
2016-12-31 20:14:00 +0200

    Initial commit

---
+++ wp-content/themes/tax/content.php
@@ -0,0 +1,363 @@
+<!-- Content -->
+<main>
+    <section class="head-block">
+        <div class="wrap">
+            <h1>Бизнес — <br> в любой точке мира!</h1>
+            <a href="#">узнать как</a>
+            <div class="icon-scroll"></div>
+        </div>
+    </section>
+    <section class="our-services">
+        <div class="wrap">
+            <div class="title">
+                <?php
+                $args_our_services = array(
+                    'post_type' => 'our_services', //slag
+                    'posts_per_page' => 10,
+                );
+                $our_services = new WP_Query($args_our_services);
+
+                //loop
+                $i = 0;
+                $ix = 0;
+                if ($our_services->have_posts()) :
+                    $result = object_to_array($our_services);
+                    foreach ($result['posts'] as $item) {
+                        //display title
+                        if ($item['post_content'] == 'Заголовок') {
+                            if ($i == 0) {
+                                echo $item['post_title'];
+                                echo "</div>";
+                            }
+                            $i++;
+                        }
+                        if ($item['post_title'] == 'Описание') {
+                            if (($i > 0) && ($ix == 0)) {
+                                echo "<div class='text'>";
+                                echo $item['post_content'];
+                                echo "</div>";
+                                echo "<div class='services'>";
+                            }
+                            $ix++;
+                        }
+                    }
+                endif;
+                wp_reset_postdata(); // return global variables to state of main query
+                $args_our_services = array(
+                    'post_type' => 'our_services', //slag
+                    'posts_per_page' => 10,
+                    'meta_key' => 'wpcf-number_our_services',
+                    'orderby' => 'meta_value',
+                    'order' => 'ASC'
+                );
+                $our_services = new WP_Query($args_our_services);
+                if ($our_services->have_posts()) :
+                    while ($our_services->have_posts()) :
+                        $our_services->the_post();
+                        //display benefits
+                        if (!get_the_content()) {
+                            echo "<div class='item'>";
+                            the_post_thumbnail();
+                            echo " <p><span>";
+                            echo types_render_field("number_our_services", array("style" => "FIELD_NAME : $ FIELD_VALUE"));
+                            echo "</span>";
+                            the_title();
+                            echo "</p>";
+                            echo "</div>";
+                        }
+                    endwhile;
+                endif;
+                wp_reset_postdata(); // return global variables to state of main query ?>
+            </div>
+    </section>
+
+    <section class="special-offer">
+        <div class="offer-title">
+            <?php
+            $args_special_offers = array(
+                'post_type' => 'special_offers', //slag
+                'posts_per_page' => 10,
+            );
+            $special_offers = new WP_Query($args_special_offers);
+
+            //loop
+            $i = 0;
+            if ($special_offers->have_posts()) :
+                while ($special_offers->have_posts()) :
+                    $result = object_to_array($special_offers);
+                    foreach ($result['posts'] as $item) {
+                        //display title
+                        if ($item['post_content'] == 'Заголовок') {
+                            if ($i == 0) {
+                                echo $item['post_title'];
+                                echo "</div>";
+                                echo "<div class='wrap'>";
+                                echo "<div class='owl-carousel carousel-1'>";
+                            }
+                            $i++;
+                        }
+                    }
+                    $special_offers->the_post();
+                    //display benefits
+                    if (!get_the_content()) {
+                        echo "<div class='item'>";
+                        echo "<div class='item-wrap'><p>";
+                        the_title();
+                        echo "</p>";
+                        echo "<a href='#'>ПОДРОБНЕЕ<img src='";
+                        echo bloginfo('template_url') . "/img/right-arrow.png' alt=''></a></div></div>";
+                    }
+                endwhile;
+            endif;
+            wp_reset_postdata(); // return global variables to state of main query ?>
+        </div>
+        </div>
+    </section>
+    <section class="results">
+        <div class="wrap">
+            <div class="results-title">РЕЗУЛЬТАТЫ В ЦИФРАХ</div>
+            <div class="result-items">
+                <div class="item">
+                    <div class="number">20</div>
+                    <p>Более 20 лет успешного опытав сфере регистрации и обслуживания компаний </p>
+                    <div class="number">01</div>
+                    <p>Офис в мировом финансовом центре в Лондоне, компании в Лондоне за 1 час
+                    </p>
+                </div>
+                <div class="item">
+                    <div class="number">72</div>
+                    <p>Возможность регистрации
+                        офшорных компаний за 72 часа
+                    </p>
+                    <div class="number">30</div>
+                    <p>Возможность регистрации компании
+                        в более 30 различных странах мира
+                    </p>
+                </div>
+                <div class="item bank">
+                    <img src="<?php bloginfo('template_url') ?>/img/bank.png" alt="">
+                    <p>Возможность открытия счета в самых крупных мировых банках</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="our-work">
+        <div class="wrap">
+            <div class="title">КАК МЫ РАБОТАЕМ</div>
+            <div class="work-item">
+                <div class="item">
+                    <div><img src="<?php bloginfo('template_url') ?>/img/h-work-1.png" alt=""></div>
+                    <div class="shema">
+                        <div class="circle"></div>
+                        <div class="rectangle"></div>
+                    </div>
+                    <p>Определяем цели и задачи<br> для регистрации компании</p>
+                </div>
+                <div class="item">
+                    <div><img src="<?php bloginfo('template_url') ?>/img/h-work-2.png" alt=""></div>
+                    <div class="shema">
+                        <div class="circle"></div>
+                        <div class="rectangle"></div>
+                    </div>
+                    <p>Выбираем название и<br> юрисдикцию компании</p>
+                </div>
+                <div class="item">
+                    <div><img src="<?php bloginfo('template_url') ?>/img/h-work-3.png" alt=""></div>
+                    <div class="shema">
+                        <div class="circle"></div>
+                        <div class="rectangle"></div>
+                    </div>
+                    <p>Консультируем в отношении<br> ведения бухгалтерии и<br> налогового планирования</p>
+                </div>
+                <div class="item">
+                    <div><img src="<?php bloginfo('template_url') ?>/img/h-work-4.png" alt=""></div>
+                    <div class="shema">
+                        <div class="circle"></div>
+                        <div class="rectangle"></div>
+                    </div>
+                    <p>Выбираем банк для<br>
+                        открытия счета</p>
+                </div>
+                <div class="item">
+                    <div><img src="<?php bloginfo('template_url') ?>/img/h-work-5.png" alt=""></div>
+                    <div class="shema">
+                        <div class="circle"></div>
+                        <div class="rectangle"></div>
+                    </div>
+                    <p>Консультируем в отношении<br> дальнейшего <br>администрирования компании</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="advanteges">
+        <div class="wrap">
+            <div class="item">
+                <div class="title">
+                    <?php
+                    $args_flavor = array(
+                        'post_type' => 'flavor', //slag
+                        'posts_per_page' => 8,
+                    );
+                    $flavor = new WP_Query($args_flavor);
+
+                    //loop
+                    $i = 0;
+                    if ($flavor->have_posts()) :
+                        $result = object_to_array($flavor);
+                        foreach ($result['posts'] as $item) {
+                            //display title
+                            if ($item['post_content'] == 'Заголовок') {
+                                if ($i == 0) {
+                                    echo $item['post_title'];
+                                    echo "</div>";
+                                }
+                                $i++;
+                            }
+                        }
+                    endif;
+                    wp_reset_postdata(); // return global variables to state of main query
+                    $args_flavor = array(
+                        'post_type' => 'flavor', //slag
+                        'posts_per_page' => 10,
+                        'meta_key' => 'wpcf-number_flv',
+                        'orderby' => 'meta_value',
+                        'order' => 'ASC'
+                    );
+                    $flavor = new WP_Query($args_flavor);
+                    if ($flavor->have_posts()) :
+                        while ($flavor->have_posts()) :
+                            $flavor->the_post();
+                            //display benefits
+                            if (!get_the_content()) {
+                                echo "<p><span>";
+                                echo types_render_field("number_flv", array("style" => "FIELD_NAME : $ FIELD_VALUE"));
+                                echo "</span>";
+                                the_title();
+                                echo "</p>";
+                            }
+                        endwhile;
+                    endif;
+                    wp_reset_postdata(); // return global variables to state of main query ?>
+                </div>
+            </div>
+    </section>
+    <section class="helpful-info">
+        <div class="wrap">
+            <div class="title">
+                <?php
+                $args_helpful_information = array(
+                    'post_type' => 'helpful_information', //slag
+                    'posts_per_page' => 10,
+                );
+                $_helpful_information = new WP_Query($args_helpful_information);
+
+                //loop
+                $i = 0;
+                if ($_helpful_information->have_posts()) :
+                    while ($_helpful_information->have_posts()) :
+                        $result = object_to_array($_helpful_information);
+                        foreach ($result['posts'] as $item) {
+                            //display title
+                            if ($item['post_content'] == 'Заголовок') {
+                                if ($i == 0) {
+                                    echo $item['post_title'];
+                                    echo "</div>";
+                                    echo "<div class='info-item'>";
+                                }
+                                $i++;
+                            }
+                        }
+                        $_helpful_information->the_post();
+                        //display benefits
+                        if (!get_the_content()) {
+                            echo "<div class='item'>";
+                            echo "<a href='#'>";
+                            the_title();
+                            echo "</a></div>";
+                        }
+                    endwhile;
+                endif;
+                wp_reset_postdata(); // return global variables to state of main query ?>
+
+            </div>
+            <div class="see-more">
+                <a href="#">ПОКАЗАТЬ БОЛЬШЕ</a>
+            </div>
+        </div>
+    </section>
+    <section class="feadback-form">
+        <div class="form-wrap">
+            <div class="title-form">НАПИСАТЬ НАМ</div>
+            <form action="">
+                <input type="text" placeholder="Имя">
+                <input type="email" placeholder="Email">
+                <textarea name="" placeholder="Текст"></textarea>
+                <input type="submit" value="Отправить">
+            </form>
+        </div>
+    </section>
+    <section class="news-front">
+        <div class="title">НОВОСТИ</div>
+        <div class="wrap">
+            <div class="owl-carousel carousel-2">
+                <div class="item">
+                    <div class="item-wrap">
+                        <div class="date">29 июня 2016</div>
+                        <p>Британские Виргинские о-ва: новые требований для Регулируемых компаний и Взаимных фондов</p>
+                        <a href="#"></a>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="item-wrap">
+                        <div class="date">27 января 2016</div>
+                        <p>Британские Виргинские о-ва: новые требований для Регулируемых компаний и Взаимных фондов</p>
+                        <a href="#"></a>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="item-wrap">
+                        <div class="date">29 июня 2016</div>
+                        <p>Британские Виргинские о-ва: новые требований для Регулируемых компаний и Взаимных фондов</p>
+                        <a href="#"></a>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="item-wrap">
+                        <div class="date">27 января 2016</div>
+                        <p>Британские Виргинские о-ва: новые требований для Регулируемых компаний и Взаимных фондов</p>
+                        <a href="#"></a>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="item-wrap">
+                        <div class="date">29 июня 2016</div>
+                        <p>Британские Виргинские о-ва: новые требований для Регулируемых компаний и Взаимных фондов</p>
+                        <a href="#"></a>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="item-wrap">
+                        <div class="date">27 января 2016</div>
+                        <p>Британские Виргинские о-ва: новые требований для Регулируемых компаний и Взаимных фондов</p>
+                        <a href="#"></a>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="item-wrap">
+                        <div class="date">29 июня 2016</div>
+                        <p>Британские Виргинские о-ва: новые требований для Регулируемых компаний и Взаимных фондов</p>
+                        <a href="#"></a>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="item-wrap">
+                        <div class="date">27 января 2016</div>
+                        <p>Британские Виргинские о-ва: новые требований для Регулируемых компаний и Взаимных фондов</p>
+                        <a href="#"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+<!-- End content -->
---
+++ wp-content/themes/tax/header.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package tax
+ */
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+
+	<meta charset="utf-8">
+
+	<title><?php bloginfo('name') ?></title>
+	<meta name="description" content="<?php bloginfo('description') ?>">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- Template Basic Images Start -->
+	<meta property="og:image" content="path/to/image.jpg">
+	<link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon" href="img/favicon/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png">
+	<!-- Template Basic Images End -->
+
+	<!-- Bootstrap (latest) Grid Styles Only -->
+
+	<!-- Load CSS, CSS Localstorage & WebFonts Main Function -->
+	<script>!function (e) {
+			"use strict";
+			function t(e, t, n) {
+				e.addEventListener ? e.addEventListener(t, n, !1) : e.attachEvent && e.attachEvent("on" + t, n)
+			};
+			function n(t, n) {
+				return e.localStorage && localStorage[t + "_content"] && localStorage[t + "_file"] === n
+			};
+			function a(t, a) {
+				if (e.localStorage && e.XMLHttpRequest)n(t, a) ? o(localStorage[t + "_content"]) : l(t, a); else {
+					var s = r.createElement("link");
+					s.href = a, s.id = t, s.rel = "stylesheet", s.type = "text/css", r.getElementsByTagName("head")[0].appendChild(s), r.cookie = t
+				}
+			}
+
+			function l(e, t) {
+				var n = new XMLHttpRequest;
+				n.open("GET", t, !0), n.onreadystatechange = function () {
+					4 === n.readyState && 200 === n.status && (o(n.responseText), localStorage[e + "_content"] = n.responseText, localStorage[e + "_file"] = t)
+				}, n.send()
+			}
+
+			function o(e) {
+				var t = r.createElement("style");
+				t.setAttribute("type", "text/css"), r.getElementsByTagName("head")[0].appendChild(t), t.styleSheet ? t.styleSheet.cssText = e : t.innerHTML = e
+			}
+
+			var r = e.document;
+			e.loadCSS = function (e, t, n) {
+				var a, l = r.createElement("link");
+				if (t)a = t; else {
+					var o;
+					o = r.querySelectorAll ? r.querySelectorAll("style,link[rel=stylesheet],script") : (r.body || r.getElementsByTagName("head")[0]).childNodes, a = o[o.length - 1]
+				}
+				var s = r.styleSheets;
+				l.rel = "stylesheet", l.href = e, l.media = "only x", a.parentNode.insertBefore(l, t ? a : a.nextSibling);
+				var c = function (e) {
+					for (var t = l.href, n = s.length; n--;)if (s[n].href === t)return e();
+					setTimeout(function () {
+						c(e)
+					})
+				};
+				return l.onloadcssdefined = c, c(function () {
+					l.media = n || "all"
+				}), l
+			}, e.loadLocalStorageCSS = function (l, o) {
+				n(l, o) || r.cookie.indexOf(l) > -1 ? a(l, o) : t(e, "load", function () {
+					a(l, o)
+				})
+			}
+		}(this);</script>
+
+
+	<!-- Load Fonts CSS End -->
+
+	<!-- Load Custom CSS Compiled without JS Start -->
+	<noscript>
+		<link rel="stylesheet" href="<?php bloginfo('template_url') ?>/css/fonts.min.css">
+		<link rel="stylesheet" href="<?php bloginfo('template_url') ?>/css/main.min.css">
+	</noscript>
+	<!-- Load Custom CSS Compiled without JS End -->
+
+	<!-- Custom Browsers Color Start -->
+	<!-- Chrome, Firefox OS and Opera -->
+	<meta name="theme-color" content="#000">
+	<!-- Windows Phone -->
+	<meta name="msapplication-navbutton-color" content="#000">
+	<!-- iOS Safari -->
+	<meta name="apple-mobile-web-app-status-bar-style" content="#000">
+	<!-- Custom Browsers Color End -->
+
+	<?php wp_head(); ?>
+</head>
+
+<body>
+
+<?php home_url() ?>
+
+<?php //bloginfo('template_url') ?>
+
+<!-- Custom HTML -->
+<!-- Header -->
+<header>
+	<div class="wrap">
+		<div class="left-side">
+			<a class="logo" href="<?php echo home_url() ?>"><img src="<?php bloginfo('template_url') ?>/img/logo.png" alt=""></a>
+			<div class="phone">
+				<p><i class="fa fa-phone" aria-hidden="true"></i>+38 (044) 249-72-22</p>
+				<a href="#">Закзазать звонок</a>
+			</div>
+		</div>
+		<div class="menu">
+			<!--<ul>
+				<li><a href="index.html">ГЛАВНАЯ</a></li>
+				<li><a href="about.html">О ПРОЕКТЕ</a></li>
+				<li><a href="services.html">Услуги и цены</a></li>
+				<li><a href="contacts.html">Контакты</a></li>
+			</ul>-->
+			<?php wp_nav_menu(array('theme_location' => 'menu-1', 'container' => 'false')); ?>
+		</div>
+	</div>
+</header>
+<!-- End header -->
